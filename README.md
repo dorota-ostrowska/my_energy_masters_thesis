e95e7fb8a08b46d9668e7b0c8fe999b857710527
dorota <dorotaostrowska.666@gmail.com>
2024-03-25 23:22:54 +0100

    readme & refactor of generator

---
+++ README.md
@@ -0,0 +1,33 @@
+<div align="center">
+  <br>
+  <h1>MyEnergy 💡🐷💸</h1>
+  <strong>Act locally, think globally: save energy!</strong>
+</div>
+<br>
+<p align="center">
+    <img src=data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNDQuMjUwMDA5NTM2NzQzMTYiIGhlaWdodD0iMzUiIHZpZXdCb3g9IjAgMCAxNDQuMjUwMDA5NTM2NzQzMTYgMzUiPjxyZWN0IHdpZHRoPSIxMDYuNjA5MzgyNjI5Mzk0NTMiIGhlaWdodD0iMzUiIGZpbGw9IiMzMUM0RjMiLz48cmVjdCB4PSIxMDYuNjA5MzgyNjI5Mzk0NTMiIHdpZHRoPSIzNy42NDA2MjY5MDczNDg2MyIgaGVpZ2h0PSIzNSIgZmlsbD0iI2VkZjkyNyIvPjx0ZXh0IHg9IjUzLjMwNDY5MTMxNDY5NzI2NiIgeT0iMjEuNSIgZm9udC1zaXplPSIxMiIgZm9udC1mYW1pbHk9IidSb2JvdG8nLCBzYW5zLXNlcmlmIiBmaWxsPSIjRkZGRkZGIiB0ZXh0LWFuY2hvcj0ibWlkZGxlIiBsZXR0ZXItc3BhY2luZz0iMiI+TUFERSBXSVRIPC90ZXh0Pjx0ZXh0IHg9IjEyNS40Mjk2OTYwODMwNjg4NSIgeT0iMjEuNSIgZm9udC1zaXplPSIxMiIgZm9udC1mYW1pbHk9IidNb250c2VycmF0Jywgc2Fucy1zZXJpZiIgZmlsbD0iI2YyZjcxNSIgdGV4dC1hbmNob3I9Im1pZGRsZSIgZm9udC13ZWlnaHQ9IjkwMCIgbGV0dGVyLXNwYWNpbmc9IjIiPvCfkI08L3RleHQ+PC9zdmc+>
+    <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNDUuMzc1IiBoZWlnaHQ9IjM1IiB2aWV3Qm94PSIwIDAgMTQ1LjM3NSAzNSI+PHJlY3Qgd2lkdGg9IjEwNi44OTA2MjUiIGhlaWdodD0iMzUiIGZpbGw9IiNkZjMxZjMiLz48cmVjdCB4PSIxMDYuODkwNjI1IiB3aWR0aD0iMzguNDg0Mzc1IiBoZWlnaHQ9IjM1IiBmaWxsPSIjNzUyN2Y5Ii8+PHRleHQgeD0iNTMuNDQ1MzEyNSIgeT0iMjEuNSIgZm9udC1zaXplPSIxMiIgZm9udC1mYW1pbHk9IidSb2JvdG8nLCBzYW5zLXNlcmlmIiBmaWxsPSIjRkZGRkZGIiB0ZXh0LWFuY2hvcj0ibWlkZGxlIiBsZXR0ZXItc3BhY2luZz0iMiI+QlVJTFQgV0lUSDwvdGV4dD48dGV4dCB4PSIxMjYuMTMyODEyNSIgeT0iMjEuNSIgZm9udC1zaXplPSIxMiIgZm9udC1mYW1pbHk9IidNb250c2VycmF0Jywgc2Fucy1zZXJpZiIgZmlsbD0iI2YyZjcxNSIgdGV4dC1hbmNob3I9Im1pZGRsZSIgZm9udC13ZWlnaHQ9IjkwMCIgbGV0dGVyLXNwYWNpbmc9IjIiPuKdpO+4jzwvdGV4dD48L3N2Zz4=">
+    <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMjIuNTQ2ODc1IiBoZWlnaHQ9IjM1IiB2aWV3Qm94PSIwIDAgMTIyLjU0Njg3NSAzNSI+PHJlY3Qgd2lkdGg9Ijg0LjkwNjI1IiBoZWlnaHQ9IjM1IiBmaWxsPSIjMjhjYjIxIi8+PHJlY3QgeD0iODQuOTA2MjUiIHdpZHRoPSIzNy42NDA2MjUiIGhlaWdodD0iMzUiIGZpbGw9IiNmOTI3M2EiLz48dGV4dCB4PSI0Mi40NTMxMjUiIHk9IjIxLjUiIGZvbnQtc2l6ZT0iMTIiIGZvbnQtZmFtaWx5PSInUm9ib3RvJywgc2Fucy1zZXJpZiIgZmlsbD0iI0ZGRkZGRiIgdGV4dC1hbmNob3I9Im1pZGRsZSIgbGV0dGVyLXNwYWNpbmc9IjIiPlRPIFNBVkU8L3RleHQ+PHRleHQgeD0iMTAzLjcyNjU2MjUiIHk9IjIxLjUiIGZvbnQtc2l6ZT0iMTIiIGZvbnQtZmFtaWx5PSInTW9udHNlcnJhdCcsIHNhbnMtc2VyaWYiIGZpbGw9IiNmMmY3MTUiIHRleHQtYW5jaG9yPSJtaWRkbGUiIGZvbnQtd2VpZ2h0PSI5MDAiIGxldHRlci1zcGFjaW5nPSIyIj7wn4yOPC90ZXh0Pjwvc3ZnPg==">
+</p>
+
+Welcome to my repository housing the code for my master's thesis in Computer Science at PJAIT (Warsaw, Poland). This platform presents an excellent opportunity for me to hone my Fullstack skills while aspiring to make a positive impact on our planet.
+
+## What is MyEnergy?
+
+We are MyEnergy, more than just an electricity seller. Our mission is to empower you to **create a better world for yourself and future generations**. Did you know that the past decade is likely to have been the hottest period in the last 125,000 years? The production of electricity has a huge impact on our environment. This environmental fact underscores the urgency of our mission. By adopting sustainable habits like acting locally and thinking globally, we can mitigate these effects. We're here to assist you in your daily energy-saving efforts. Let's make a difference together – **YOU and MyEnergy**!
+
+## Table of contents
+
+- [What is MyEnergy?](#what-is-myenergy)
+- [Table of contents](#table-of-contents)
+- [Technologies](#technologies)
+- [Getting started](#getting-started)
+
+## Technologies
+* Python
+
+## Getting started
+
+...
+
+[⬆ Back to top](#table-of-contents)
--- consumption_generator.py
+++ consumption_generator.py
@@ -1,87 +1,107 @@
 import psycopg2
 import json
 import numpy as np
-import matplotlib.pyplot as plt
 import random
 import datetime
-from typing import List, Optional, Dict
+from typing import List, Optional, Dict, Tuple
 
-def generate_energy_consumption(hour: int) -> float:
-    """
-    Function generating energy consumption for a given hour (in kW).
-    """
-    amplitude: float = 0.3
-    period: int = 24  
-    offset: float = 0.3  
-    energy_usage: float = amplitude * np.sin(2 * np.pi * hour / period) + offset
-    return energy_usage
+class ConsumptionGenerator:
+    def __init__(self, reading_date: datetime.datetime) -> None:
+        self.db_connection: Optional[psycopg2.extensions.connection] 
+        self.db_cursor: Optional[psycopg2.extensions.cursor] 
+        self.db_connection, self.db_cursor = self._open_connection_with_db()
+        if self.db_connection and self.db_cursor:
+            self.generate_readings(reading_date)
+        self._close_connection_with_db()
 
-def add_noise() -> float:
-    """
-    Introduces noise into the electricity consumption profile for a given household.
-    """
-    return random.uniform(-0.2, 0.2)
+    def _open_connection_with_db(self) -> Tuple[Optional[psycopg2.extensions.cursor], 
+                                                Optional[psycopg2.extensions.cursor]]:
+        """
+        Opens a connection with PostgreSQL database.
+        """
+        try:
+            with open("appconfig.json", "r") as file:
+                json_data: Dict = json.load(file)
+            db_connection: psycopg2.extensions.connection = psycopg2.connect(**json_data)
+            db_cursor: psycopg2.extensions.cursor = db_connection.cursor()
+            print("PostgreSQL connection is opened")
+            return db_connection, db_cursor
+        except (Exception) as error:
+            print("Failed to connect to database,", error)
+            return None, None
+        
+    def _close_connection_with_db(self) -> None:
+        """
+        Closes a connection with PostgreSQL database.
+        """
+        if self.db_connection: 
+            self.db_cursor.close()
+            self.db_connection.close()
+            print("PostgreSQL connection is closed")
 
-def get_meter_ids(cursor) -> List[int]:
-    query: str = "SELECT id_meter FROM meter"
-    cursor.execute(query)
-    return cursor.fetchall()
+    def generate_energy_consumption(self, hour_of_reading: int) -> float:
+        """
+        Function generating energy consumption for a given hour (in kW).
+        """
+        amplitude: float = 0.3
+        period: int = 24  
+        offset: float = 0.3  
+        energy_usage: float = amplitude * np.sin(2 * np.pi * hour_of_reading / period) + offset
+        return energy_usage
 
-def get_the_biggest_id_from_id_reading() -> int:
-    """
-    Gets the biggest ID from the Reading table.
-    If there is no readings, it returns 0 and the first reading will have ID equals 1.
-    """
-    query: str = "SELECT MAX(id_reading) FROM reading;"
-    cursor.execute(query)
-    fetched: Optional[int] = cursor.fetchone()[0]
-    return fetched if fetched else 0  
+    def generate_noise(self) -> float:
+        """
+        Introduces noise into the electricity consumption profile for a given household.
+        """
+        return random.uniform(-0.2, 0.2)    
 
-def add_reading(id_reading: int, time: str, used_energy: float, meter_id_meter: int) -> None:
-    """
-    Adds reading to the Reading table.
-    """
-    try: 
-        query: str = f"INSERT INTO reading(id_reading, time, used_energy, meter_id_meter) VALUES ({id_reading}, '{time}', {used_energy}, {meter_id_meter});"
-        cursor.execute(query)
-        conn.commit()
-        print("Record inserted successfully into the Reading table")
-    except (Exception, psycopg2.Error) as error:
-        print("Failed to insert record into the Reading table", error)
+    def get_all_meters(self) -> List[int]:
+        """
+        Gets all ID's to generate readings for them. 
+        """
+        query: str = "SELECT id_meter FROM meter"
+        self.db_cursor.execute(query)
+        return self.db_cursor.fetchall()
 
-def show_consumption_profile() -> None:
-    hours = np.arange(24)
-    energy_consumption = [max(0, (generate_energy_consumption(hour) + add_noise())) for hour in hours]
-    plt.plot(hours, energy_consumption, marker='o')
-    plt.title('Energy consumption for a household (example)')
-    plt.xlabel('hour')
-    plt.ylabel('used power [kW]')
-    plt.grid(True)
-    plt.xticks(np.arange(0, 24, step=1))
-    plt.show()
+    def get_the_latest_reading(self) -> int:
+        """
+        Gets the biggest ID from the Reading table.
+        If there is no readings, it returns 0 and the first reading will have ID equals 1.
+        """
+        query: str = "SELECT MAX(id_reading) FROM reading;"
+        self.db_cursor.execute(query)
+        fetched: Optional[int] = self.db_cursor.fetchone()[0]
+        return fetched if fetched else 0  
 
-try:
-    with open("appconfig.json", "r") as file:  # open a connection
-        json_data: Dict = json.load(file)
-    conn: psycopg2.extensions.connection = psycopg2.connect(**json_data)
-    cursor: psycopg2.extensions.cursor = conn.cursor()
-    print("PostgreSQL connection is opened")
-except (Exception, psycopg2.Error) as error:
-    print("Failed to connect to database", error)
-all_meters: List[int] = get_meter_ids(cursor)
-biggest_id: int = get_the_biggest_id_from_id_reading()
-current_timestamp: datetime = datetime.datetime.now()
-for meter in all_meters:
-    biggest_id += 1
-    add_reading(
-        biggest_id, 
-        str(current_timestamp),
-        max(0, (generate_energy_consumption(current_timestamp.hour) + add_noise())),
-        meter[0]
-        )
-if conn:  # close a connection
-    cursor.close()
-    conn.close()
-    print("PostgreSQL connection is closed")
+    def add_reading_to_table(self, id_reading: int, time: str, used_energy: float, meter_id_meter: int) -> None:
+        """
+        Adds reading to the Reading table.
+        """
+        try: 
+            query: str = f"INSERT INTO reading(id_reading, time, used_energy, meter_id_meter) VALUES ({id_reading}, '{str(time)}', {used_energy}, {meter_id_meter});"
+            self.db_cursor.execute(query)
+            print("Record inserted successfully into the Reading table")
+        except (Exception) as error:
+            print("Failed to insert record into the Reading table,", error)
 
-show_consumption_profile()
+    def generate_readings(self, reading_date: datetime.datetime) -> None:
+        """
+        Generates reading for each meter in db. Commits transaction at the end.
+        """
+        all_meters: List[int] = self.get_all_meters()
+        last_reading_id: int = self.get_the_latest_reading()
+        for meter in all_meters:
+            last_reading_id += 1
+            self.add_reading_to_table(
+                last_reading_id, 
+                reading_date,
+                max(0, (self.generate_energy_consumption(reading_date.hour) + self.generate_noise())),
+                meter[0]
+                )
+        self.db_connection.commit()
+
+dt: datetime = datetime.datetime(2020, 2, 12)
+tm: datetime = datetime.time(1, 0)
+combined_date: datetime = dt.combine(dt, tm)
+timestamp: datetime = datetime.datetime.now()
+consumption_generator: ConsumptionGenerator = ConsumptionGenerator(combined_date)
