6162f75dce04ccc9063ad67a8206086ad539c0ef
dorota <dorotaostrowska.666@gmail.com>
2024-04-21 22:34:18 +0200

    badges added as classes & story added

---
+++ game/badges.py
@@ -0,0 +1,28 @@
+class EmeraldLeaf:
+    name = "Emerald Leaf"
+    points = 1
+    randomness = 0.4
+
+
+class GeothermalRuby:
+    name = "Geothermal Ruby"
+    points = 2
+    randomness = 0.25
+
+
+class SapphireWave:
+    name = "Sapphire Wave"
+    points = 5
+    randomness = 0.2
+
+
+class GoldenSunbeam:
+    name = "Golden Sunbeam"
+    points = 13
+    randomness = 0.1
+
+
+class DiamonsDraught:
+    name = "Diamons Draught"
+    points = 34
+    randomness = 0.05
