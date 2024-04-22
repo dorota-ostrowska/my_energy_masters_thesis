b03e6f02ac2ff14596bcef95ef5cc6c73f3ed927
dorota <dorotaostrowska.666@gmail.com>
2024-04-22 18:53:41 +0200

    first small challange added

---
+++ game/challenges/small_challenges/5/__init__.py
@@ -0,0 +1,39 @@
+import requests
+from secret import WEATHER_API_KEY
+
+def _check_weather(city: str) -> str:
+    """
+    Returns a string with information if the current weather conditions are OK to 
+    dry user's washing ourside. It uses a city name to get this info.
+    """
+    base_url: str = "http://api.weatherapi.com/v1/current.json?"
+    complete_url: str = f"{base_url}key={WEATHER_API_KEY}&q={city}"
+    response = requests.get(complete_url)
+    response_json = response.json()
+    if "error" not in response_json:
+        wind = response_json["current"]["wind_kph"]
+        humidity = response_json["current"]["humidity"]
+        temperature = response_json["current"]["temp_c"]
+        if(humidity > 60):
+            return "The weather conditions aren't good to dry your washing, it's too wet. " \
+                "Try to do this task another day! 😉"
+        elif(wind > 30):
+            return "The weather conditions aren't good to dry your washing, it's too windy. " \
+                "Try to do this task another day! 😉"
+        elif(temperature > 10):
+            return "The weather conditions aren't good to dry your washing, it's too cold. " \
+                "Try to do this task another day! 😉"
+        else:
+            return "The weather conditions are great to dry your washing 💚."
+    else:
+        return "You have to check the weather conditions on your own because there is a problem with app."
+        
+
+def get_task_description(users_city: str) -> str:
+    """
+    Returns a task description with information on weather conditions.
+    """
+    with open('game/challenges/small_challenges/5/task_description.txt', 'r', encoding="utf-8") as file:
+        raw_text = file.read()
+    complete_task = raw_text + "\nDaily weather check ⛅💦💨:\n" + _check_weather(users_city) 
+    return complete_task
