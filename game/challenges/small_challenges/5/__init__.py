725f7c1658c55b424ad90d597c1ef544dde99d81
dorota <dorotaostrowska.666@gmail.com>
2024-04-22 18:55:33 +0200

    black

--- game/challenges/small_challenges/5/__init__.py
+++ game/challenges/small_challenges/5/__init__.py
@@ -1,9 +1,10 @@
 import requests
 from secret import WEATHER_API_KEY
 
+
 def _check_weather(city: str) -> str:
     """
-    Returns a string with information if the current weather conditions are OK to 
+    Returns a string with information if the current weather conditions are OK to
     dry user's washing ourside. It uses a city name to get this info.
     """
     base_url: str = "http://api.weatherapi.com/v1/current.json?"
@@ -14,26 +15,36 @@ def _check_weather(city: str) -> str:
         wind = response_json["current"]["wind_kph"]
         humidity = response_json["current"]["humidity"]
         temperature = response_json["current"]["temp_c"]
-        if(humidity > 60):
-            return "The weather conditions aren't good to dry your washing, it's too wet. " \
+        if humidity > 60:
+            return (
+                "The weather conditions aren't good to dry your washing, it's too wet. "
                 "Try to do this task another day! 😉"
-        elif(wind > 30):
-            return "The weather conditions aren't good to dry your washing, it's too windy. " \
+            )
+        elif wind > 30:
+            return (
+                "The weather conditions aren't good to dry your washing, it's too windy. "
                 "Try to do this task another day! 😉"
-        elif(temperature > 10):
-            return "The weather conditions aren't good to dry your washing, it's too cold. " \
+            )
+        elif temperature > 10:
+            return (
+                "The weather conditions aren't good to dry your washing, it's too cold. "
                 "Try to do this task another day! 😉"
+            )
         else:
             return "The weather conditions are great to dry your washing 💚."
     else:
         return "You have to check the weather conditions on your own because there is a problem with app."
-        
+
 
 def get_task_description(users_city: str) -> str:
     """
     Returns a task description with information on weather conditions.
     """
-    with open('game/challenges/small_challenges/5/task_description.txt', 'r', encoding="utf-8") as file:
+    with open(
+        "game/challenges/small_challenges/5/task_description.txt", "r", encoding="utf-8"
+    ) as file:
         raw_text = file.read()
-    complete_task = raw_text + "\nDaily weather check ⛅💦💨:\n" + _check_weather(users_city) 
+    complete_task = (
+        raw_text + "\nDaily weather check ⛅💦💨:\n" + _check_weather(users_city)
+    )
     return complete_task
